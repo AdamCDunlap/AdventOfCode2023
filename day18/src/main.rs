8ee662f3ee2914fc8560611dd7972c8095ed2786
Adam Dunlap <dunlapadam@gmail.com>
2023-12-18 16:21:42 -0800

    Partial day 18, part 2 solution with Lines having dirs

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -7,12 +7,190 @@ enum Dir {
 }
 use std::{
     collections::VecDeque,
-    fmt::Write,
-    ops::{Add, Index, IndexMut},
+    ops::{Add, AddAssign, Mul},
     str::FromStr,
 };
 
 use Dir::*;
+
+#[derive(Clone)]
+struct Line {
+    dir: Dir,
+    len: usize,
+    start: Coord,
+}
+
+impl Line {
+    fn left_side(&self) -> isize {
+        match self.dir {
+            Up | Down => panic!("can only be called on a horizontal line"),
+            Left => self.start.x - self.len as isize,
+            Right => self.start.x,
+        }
+    }
+    fn right_side(&self) -> isize {
+        match self.dir {
+            Up | Down => panic!("can only be called on a horizontal line"),
+            Left => self.start.x,
+            Right => self.start.x + self.len as isize,
+        }
+    }
+}
+
+struct Directions {
+    lines: Vec<Line>,
+    width: usize,
+    height: usize,
+}
+
+impl Directions {
+    fn from_part1_str(input: &str) -> Self {
+        Self::from_line_segments(
+            input
+                .lines()
+                .map(|l| {
+                    let strs: [&str; 3] = l
+                        .split_whitespace()
+                        .collect::<Vec<&str>>()
+                        .try_into()
+                        .unwrap();
+                    let dir: Dir = strs[0].parse().unwrap();
+                    let len: usize = strs[1].parse().unwrap();
+                    Line {
+                        dir,
+                        len,
+                        start: Coord {
+                            x: isize::MIN,
+                            y: isize::MIN,
+                        },
+                    }
+                })
+                .collect(),
+        )
+    }
+    fn from_part2_str(input: &str) -> Self {
+        Self::from_line_segments(
+            input
+                .lines()
+                .map(|line| {
+                    let dir_code = line.as_bytes()[line.len() - 2];
+                    let len =
+                        usize::from_str_radix(&line[line.len() - 7..line.len() - 2], 16).unwrap();
+                    let dir = match dir_code {
+                        b'0' => Right,
+                        b'1' => Down,
+                        b'2' => Left,
+                        b'3' => Up,
+                        _ => panic!("Invalid dir code {}", dir_code),
+                    };
+                    Line {
+                        dir,
+                        len,
+                        start: Coord {
+                            x: isize::MIN,
+                            y: isize::MIN,
+                        },
+                    }
+                })
+                .collect(),
+        )
+    }
+
+    fn from_line_segments(mut lines: Vec<Line>) -> Self {
+        let mut current_coord = Coord { x: 0, y: 0 };
+        let mut max_x = 0;
+        let mut min_x = 0;
+        let mut max_y = 0;
+        let mut min_y = 0;
+
+        for Line { dir, len, start: _ } in lines.iter() {
+            current_coord += dir.to_coord() * *len as isize;
+            min_x = std::cmp::min(min_x, current_coord.x);
+            min_y = std::cmp::min(min_y, current_coord.y);
+            max_x = std::cmp::max(max_x, current_coord.x);
+            max_y = std::cmp::max(max_y, current_coord.y);
+        }
+
+        assert_eq!(current_coord, Coord { x: 0, y: 0 });
+
+        let mut current_coord = Coord {
+            x: -min_x,
+            y: -min_y,
+        };
+
+        for Line { dir, len, start } in lines.iter_mut() {
+            *start = current_coord.clone();
+            current_coord += dir.to_coord() * *len as isize;
+        }
+
+        Self {
+            lines,
+            width: (max_x - min_x + 1) as usize,
+            height: (max_y - min_y + 1) as usize,
+        }
+    }
+
+    // fn calculate_dimensions(&self) -> Coord {
+    //     let mut current_coord = Coord { x: 0, y: 0 };
+    //     let mut max_x = 0;
+    //     let mut min_x = 0;
+    //     let mut max_y = 0;
+    //     let mut min_y = 0;
+
+    //     for LineSegment { dir, len } in self.0.iter() {
+    //         current_coord += dir.to_coord() * *len as isize;
+    //         min_x = std::cmp::min(min_x, current_coord.x);
+    //         min_y = std::cmp::min(min_y, current_coord.y);
+    //         max_x = std::cmp::max(max_x, current_coord.x);
+    //         max_y = std::cmp::max(max_y, current_coord.y);
+    //     }
+
+    //     assert_eq!(current_coord, Coord { x: 0, y: 0 });
+
+    //     Coord {
+    //         x: max_x - min_x + 1,
+    //         y: max_y - min_y + 1,
+    //     }
+    // }
+
+    fn always_turns(&self) -> bool {
+        self.lines.windows(2).all(|win| win[0].dir != win[1].dir)
+    }
+
+    fn horizontal_lines_crossing_x(&self, x: isize) -> impl Iterator<Item = &Line> {
+        self.lines.iter().filter(move |line| match line.dir {
+            Up | Down => false,
+            Left | Right => line.left_side() <= x && x < line.right_side(),
+        })
+    }
+
+    fn get_enclosed_area(&self) -> usize {
+        let mut segments_to_consider = self.lines.clone();
+        let mut area = 0;
+        while let Some(segment_to_consider) = segments_to_consider.pop() {
+            let start_x = segment_to_consider.start.x;
+
+            let mut num_above = 0;
+            let mut first_below: Option<Line> = None;
+
+            self.horizontal_lines_crossing_x(start_x)
+                .for_each(|other_line| {
+                    if other_line.start.y < segment_to_consider.start.y {
+                        num_above += 1;
+                    } else {
+                        if let Some(prev_highest) = &first_below {
+                            if other_line.start.y < prev_highest.start.y {
+                                first_below = Some(other_line.clone());
+                            }
+                        } else {
+                            first_below = Some(other_line.clone());
+                        }
+                    }
+                });
+        }
+        area
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Coord {
@@ -24,23 +202,35 @@ impl Add<Dir> for Coord {
     type Output = Coord;
 
     fn add(self, rhs: Dir) -> Self::Output {
-        match rhs {
-            Up => Self {
-                x: self.x,
-                y: self.y - 1,
-            },
-            Down => Self {
-                x: self.x,
-                y: self.y + 1,
-            },
-            Right => Self {
-                x: self.x + 1,
-                y: self.y,
-            },
-            Left => Self {
-                x: self.x - 1,
-                y: self.y,
-            },
+        rhs.to_coord() + self
+    }
+}
+
+impl Add for Coord {
+    type Output = Coord;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl AddAssign for Coord {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl Dir {
+    fn to_coord(self) -> Coord {
+        match self {
+            Up => Coord { x: 0, y: -1 },
+            Down => Coord { x: 0, y: 1 },
+            Right => Coord { x: 1, y: 0 },
+            Left => Coord { x: -1, y: 0 },
         }
     }
 }
@@ -59,227 +249,20 @@ impl FromStr for Dir {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
-enum Square {
-    Edge { color: String },
-    Inside,
-    Outside,
-}
+impl Mul<isize> for Coord {
+    type Output = Coord;
 
-struct Lake {
-    squares: VecDeque<VecDeque<Square>>,
-    x_off: isize,
-    y_off: isize,
-}
-
-impl FromStr for Lake {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Lake::from_directions(s.lines().map(|l| {
-            let strs: [&str; 3] = l
-                .split_whitespace()
-                .collect::<Vec<&str>>()
-                .try_into()
-                .unwrap();
-            let dir: Dir = strs[0].parse().unwrap();
-            let len: u64 = strs[1].parse().unwrap();
-            let color = &strs[2][1..strs[2].len() - 1];
-            (dir, len, color)
-        })))
-    }
-}
-
-impl Lake {
-    fn new() -> Self {
+    fn mul(self, rhs: isize) -> Self::Output {
         Self {
-            squares: VecDeque::new(),
-            x_off: 0,
-            y_off: 0,
+            x: self.x * rhs,
+            y: self.y * rhs,
         }
-    }
-
-    fn from_directions<'a>(it: impl Iterator<Item = (Dir, u64, &'a str)>) -> Self {
-        let mut lake = Lake::new();
-        let mut current_coord = Coord { x: 0, y: 0 };
-
-        for (dir, len, color) in it {
-            for _ in 0..len {
-                current_coord = current_coord + dir;
-                lake.make_room_for_coord(current_coord);
-                assert_eq!(lake[current_coord], Square::Outside);
-                lake[current_coord] = Square::Edge {
-                    color: color.to_string(),
-                }
-            }
-        }
-
-        lake
-    }
-
-    fn height(&self) -> isize {
-        self.squares.len() as isize
-    }
-    fn width(&self) -> isize {
-        if self.squares.is_empty() {
-            0
-        } else {
-            self.squares[0].len() as isize
-        }
-    }
-
-    fn make_room_for_coord(&mut self, coord: Coord) {
-        let mut new_row = VecDeque::new();
-        new_row.resize(self.width() as usize, Square::Outside);
-        while coord.y + self.y_off < 0 {
-            self.y_off += 1;
-            self.squares.push_front(new_row.clone());
-        }
-
-        while coord.x + self.x_off < 0 {
-            self.x_off += 1;
-            for line in self.squares.iter_mut() {
-                line.push_front(Square::Outside);
-            }
-        }
-
-        let necessary_width = coord.x + self.x_off;
-        let necessary_height = coord.y + self.y_off;
-        if necessary_height >= self.height() {
-            self.squares.resize(necessary_height as usize + 1, new_row);
-        }
-        if necessary_width >= self.width() {
-            for line in self.squares.iter_mut() {
-                line.resize(necessary_width as usize + 1, Square::Outside);
-            }
-        }
-    }
-
-    fn is_inside(&self, x: usize, y: usize) -> bool {
-        if matches!(self.squares[y][x], Square::Edge { .. }) {
-            return false;
-        }
-        if x <= 0 {
-            return false;
-        }
-        let mut seems_inside = false;
-        for test_y in 0..y {
-            if matches!(self.squares[test_y][x], Square::Edge { .. })
-                && matches!(self.squares[test_y][x - 1], Square::Edge { .. })
-            {
-                seems_inside = !seems_inside;
-            }
-        }
-
-        seems_inside
-    }
-
-    fn dig_interior(&mut self) {
-        for y in 0..self.height() as usize {
-            for x in 0..self.width() as usize {
-                if self.is_inside(x, y) {
-                    self.squares[y][x] = Square::Inside;
-                }
-            }
-        }
-    }
-
-    fn count_edge_or_inside(&self) -> usize {
-        (0..self.height() as usize)
-            .map(|y| {
-                (0..self.width() as usize)
-                    .map(|x| match self.squares[y][x] {
-                        Square::Edge { .. } | Square::Inside => 1,
-                        Square::Outside => 0,
-                    })
-                    .sum::<usize>()
-            })
-            .sum()
-    }
-}
-
-#[test]
-fn test_make_room_for_coord() {
-    let mut lake = Lake::new();
-    lake.make_room_for_coord(Coord { x: 0, y: 0 });
-    assert_eq!(lake.width(), 1);
-    assert_eq!(lake.height(), 1);
-    assert_eq!(lake.x_off, 0);
-    assert_eq!(lake.y_off, 0);
-
-    let mut lake = Lake::new();
-    lake.make_room_for_coord(Coord { x: -1, y: -1 });
-    assert_eq!(lake.width(), 1);
-    assert_eq!(lake.height(), 1);
-    assert_eq!(lake.x_off, 1);
-    assert_eq!(lake.y_off, 1);
-
-    let mut lake = Lake::new();
-    lake.make_room_for_coord(Coord { x: -50, y: -20 });
-    assert_eq!(lake.width(), 50);
-    assert_eq!(lake.height(), 20);
-    assert_eq!(lake.x_off, 50);
-    assert_eq!(lake.y_off, 20);
-
-    lake.make_room_for_coord(Coord { x: 20, y: 10 });
-    assert_eq!(lake.width(), 71);
-    assert_eq!(lake.height(), 31);
-    assert_eq!(lake.x_off, 50);
-    assert_eq!(lake.y_off, 20);
-}
-
-#[test]
-fn test_from_directions() {
-    let lake = Lake::from_directions([(Left, 2, "")].into_iter());
-    assert_eq!(lake.width(), 2);
-    assert_eq!(lake.height(), 1);
-    assert_eq!(lake.x_off, 2);
-    assert_eq!(lake.y_off, 0);
-}
-
-impl Index<Coord> for Lake {
-    type Output = Square;
-
-    fn index(&self, index: Coord) -> &Self::Output {
-        &self.squares[(index.y + self.y_off) as usize][(index.x + self.x_off) as usize]
-    }
-}
-
-impl IndexMut<Coord> for Lake {
-    fn index_mut(&mut self, index: Coord) -> &mut Self::Output {
-        &mut self.squares[(index.y + self.y_off) as usize][(index.x + self.x_off) as usize]
-    }
-}
-
-impl std::fmt::Display for Lake {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // output 0 x point
-        for _ in 0..self.x_off + 1 {
-            f.write_char(' ')?;
-        }
-        f.write_str("0\n")?;
-        for (y, row) in self.squares.iter().enumerate() {
-            f.write_char(if y as isize == self.y_off { '0' } else { ' ' })?;
-            for sq in row.iter() {
-                f.write_char(match sq {
-                    Square::Edge { .. } => '#',
-                    Square::Inside => '@',
-                    Square::Outside => '.',
-                })?;
-            }
-            f.write_char('\n')?;
-        }
-
-        Ok(())
     }
 }
 
 fn part1(input: &str) -> usize {
-    let mut lake: Lake = input.parse().unwrap();
-    println!("before digging: \n{}", lake);
-    lake.dig_interior();
-    println!("after digging: \n{}", lake);
-    lake.count_edge_or_inside()
+    let directions = Directions::from_part1_str(input);
+    directions.get_enclosed_area()
 }
 
 #[test]
@@ -287,9 +270,20 @@ fn test_part1() {
     assert_eq!(part1(TEST_INPUT), 62);
 }
 
+fn part2(input: &str) -> usize {
+    let directions = Directions::from_part2_str(input);
+    directions.get_enclosed_area()
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(TEST_INPUT), 952408144115);
+}
+
 fn main() {
     let input = &std::fs::read_to_string("input.txt").expect("input.txt should exist");
     println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
 }
 
 const TEST_INPUT: &str = r"R 6 (#70c710)
